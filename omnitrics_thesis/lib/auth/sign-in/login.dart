a581adf3827437d64b3403dec0229b165eb77681
superhoomandc <delacruzjerichobrian@gmail.com>
2025-03-04 18:46:15 +0800

    04_03_2025 DELA CRUZ ADD

--- omnitrics_thesis/lib/auth/sign-in/login.dart
+++ omnitrics_thesis/lib/auth/sign-in/login.dart
@@ -16,11 +16,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<LoginScreen> {
-  // Class for the Sign up screen
-// For the controller
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool isLoading = false;
+
   @override
   void dispose() {
     super.dispose();
@@ -29,18 +28,16 @@ class _SignupScreenState extends State<LoginScreen> {
   }
 
   void userLogin() async {
-    // Error handling to make sure that the values are not empty
     if (emailController.text.isEmpty || passwordController.text.isEmpty) {
       showSnackBar(context, "Please fill all the fields.");
+      return;
     }
-    // Signup process
     String res = await AuthServices().userLogin(
       email: emailController.text,
       password: passwordController.text,
     );
 
     if (res == "Login successful!") {
-      //MESSAGE
       showSnackBar(context, "Successfully logged in!");
       setState(() {
         isLoading = true;
@@ -58,108 +55,115 @@ class _SignupScreenState extends State<LoginScreen> {
     }
   }
 
+  void googleSignIn() async {
+    var userCredential = await FirebaseServices().signInWithGoogle();
+    
+    if (userCredential == null) {
+      // If no account was selected, we don't navigate to the homepage
+      showSnackBar(context, "Account selection canceled or failed.");
+      return;  // Don't navigate to the homepage if no account was selected
+    }
+    
+    // Only proceed to homepage if sign-in was successful
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const HomePage(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: SizedBox(
-        child: Column(
-          children: [
-            SizedBox(
-              height: height / 2.7,
-              child: Image.asset("assets/images/login.jpg"),
-            ),
-            TextFieldInput(
-                textEditingController: emailController,
-                hintText: "Enter your email",
-                icon: Icons.email),
-            TextFieldInput(
-                textEditingController: passwordController,
-                hintText: "Enter your password",
-                icon: Icons.lock),
-            const ForgotPassword(),
-            //Forget password at the top of log in
-            LoginButton(onTab: userLogin, text: "Log In"),
-            //Divider for the sign up and google log in
-            Row(
-              children: [
-                Expanded(
-                  child: Container(height: 1, color: Colors.black26),
-                ),
-                Expanded(
-                  child: Container(height: 1, color: Colors.black26),
-                )
-              ],
-            ),
-
-            //for google login
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                onPressed: () async { 
-                  await FirebaseServices().signInWithGoogle();
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const HomePage(),
-                      ));
-                },
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: Image.asset(
-                        "assets/images/googleLogo.png",
-                        height: 35,
+        child: SizedBox(
+          child: Column(
+            children: [
+              SizedBox(
+                height: height / 2.7,
+                child: Image.asset("assets/images/login.jpg"),
+              ),
+              TextFieldInput(
+                  textEditingController: emailController,
+                  hintText: "Enter your email",
+                  icon: Icons.email),
+              TextFieldInput(
+                  textEditingController: passwordController,
+                  hintText: "Enter your password",
+                  icon: Icons.lock),
+              const ForgotPassword(),
+              LoginButton(onTab: userLogin, text: "Log In"),
+              Row(
+                children: [
+                  Expanded(child: Container(height: 1, color: Colors.black26)),
+                  Expanded(child: Container(height: 1, color: Colors.black26))
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  onPressed: () async {
+                    googleSignIn(); // Call the method to handle Google sign-in
+                  },
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Image.asset(
+                          "assets/images/googleLogo.png",
+                          height: 35,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 20),
-                    const Text(
-                      "Continue with Google",
-                      style: TextStyle(
+                      const SizedBox(width: 20),
+                      const Text(
+                        "Continue with Google",
+                        style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
-                          color: Color.fromARGB(255, 77, 76, 76)),
+                          color: Color.fromARGB(255, 77, 76, 76),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 100),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignUpScreen(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        " Sign Up ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
                     )
                   ],
                 ),
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 10, left: 100),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Don't have an account?",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignUpScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      " Sign Up ",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
--- omnitrics_thesis/lib/main.dart
+++ omnitrics_thesis/lib/main.dart
@@ -1,28 +1,28 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:omnitrics_thesis/auth/sign-in/login.dart';
-import 'package:omnitrics_thesis/getStarted/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:omnitrics_thesis/auth/Services/auth_redirect.dart';
 
 
 void main() async {
-  // Initialize Firebase
+  // Ensure that the Flutter bindings are initialized before running Firebase
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
   await Firebase.initializeApp();
-  // Run App
+
+  // Run the app
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'OmniTrics',
+      home: const AuthRedirect(),  // Use AuthRedirect to handle initial login state
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Inter'),
-      home: const SplashScreen(),
     );
   }
 }
