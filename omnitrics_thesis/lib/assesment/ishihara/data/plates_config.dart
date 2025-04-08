139e0acf447b1811b974c84fb93f07bb9b89a9a8
Ram Jesler Delos Santos <ramjesler.delossantos@gmail.com>
2025-04-08 20:43:42 +0800

    Adjusted the sequence of plates

--- omnitrics_thesis/lib/assesment/ishihara/data/plates_config.dart
+++ omnitrics_thesis/lib/assesment/ishihara/data/plates_config.dart
@@ -12,63 +12,63 @@ class TestConfig {
 
 final List<TestConfig> testConfigs = [
   TestConfig(
-    imagePath: 'assets/images/ishihara/ishihara_01.jpg',
-    options: ['16', '8', '6', '15'],
+    imagePath: 'assets/images/ishihara/ishihara_01.png',
+    options: ['12', '13', '16', '15'],
     correctAnswerIndex: 0,
   ),
   TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_02.jpg',
-    options: ['42', '12', '74', '21'],
-    correctAnswerIndex: 2,
+    options: ['3', '6', '5', '7'],
+    correctAnswerIndex: 1,
   ),
   TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_03.jpg',
-    options: ['7', '6', '2', '9'],
-    correctAnswerIndex: 1,
+    options: ['0', '3', '2', '8'],
+    correctAnswerIndex: 3,
   ),
   TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_04.jpg',
     options: ['1', '5', '2', '3'],
-    correctAnswerIndex: 2,
+    correctAnswerIndex: 1,
   ),
   TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_05.jpg',
-    options: ['1', '4', '9', '7'],
-    correctAnswerIndex: 3,
+    options: ['3', '4', '5', '6'],
+    correctAnswerIndex: 0,
   ),
   TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_06.jpg',
-    options: ['5', '6', '2', '7'],
+    options: ['15', '16', '17', '19'],
     correctAnswerIndex: 0,
   ),
   TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_07.jpg',
-    options: ['8', '0', '3', '9'],
-    correctAnswerIndex: 0,
+    options: ['21', '32', '36', '74'],
+    correctAnswerIndex: 3,
   ),
   TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_08.jpg',
     options: ['8', '2', '3', '0'],
-    correctAnswerIndex: 2,
-  ),
-  TestConfig(
-    imagePath: 'assets/images/ishihara/ishihara_09.jpg',
-    options: ['29', '36', '57', '19'],
-    correctAnswerIndex: 0,
-  ),
-  TestConfig(
-    imagePath: 'assets/images/ishihara/ishihara_10.jpg',
-    options: ['75', '32', '55', '45'],
-    correctAnswerIndex: 3,
-  ),
-  TestConfig(
-    imagePath: 'assets/images/ishihara/ishihara_11.jpg',
-    options: ['67', '97', '86', '91'],
     correctAnswerIndex: 1,
   ),
   TestConfig(
+    imagePath: 'assets/images/ishihara/ishihara_09.jpg',
+    options: ['29', '45', '57', '19'],
+    correctAnswerIndex: 1,
+  ),
+  TestConfig(
+    imagePath: 'assets/images/ishihara/ishihara_10.jpg',
+    options: ['97', '32', '55', '45'],
+    correctAnswerIndex: 0,
+  ),
+  TestConfig(
+    imagePath: 'assets/images/ishihara/ishihara_11.jpg',
+    options: ['11', '13', '16', '15'],
+    correctAnswerIndex: 2,
+  ),
+  TestConfig(
     imagePath: 'assets/images/ishihara/ishihara_12.jpg',
-    options: ['72', '23', '42', '35'],
+    options: ['4', '2', '42', '24'],
     correctAnswerIndex: 2,
   ),
 ];
