cac211a741abfac0fae9d07314d272cd3c06d55e
Ram Jesler Delos Santos <ramjesler.delossantos@gmail.com>
2025-04-18 22:15:24 +0800

    Added screenutil to Exit Btn

--- omnitrics_thesis/lib/tell_me_who_you_are/Widget/exit_button.dart
+++ omnitrics_thesis/lib/tell_me_who_you_are/Widget/exit_button.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:omnitrics_thesis/auth/sign-in/sign_up.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Row exitButton() {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
     children: [
-      const Text(
+      Text(
         'Tell me who you are',
         style: TextStyle(
-          fontSize: 20,
+          fontSize: 20.sp,
           fontWeight: FontWeight.bold,
         ),
       ),
@@ -21,7 +22,7 @@ Row exitButton() {
                 MaterialPageRoute(builder: (context) => const SignUpScreen()),
               );
             },
-            padding: EdgeInsets.zero,
+            padding: EdgeInsets.zero.w,
             constraints: const BoxConstraints(),
           );
         },
